7aa5cc0a39a26169caf33325f173cbda8da322f4
Jon <jbrundagejr@gmail.com>
2021-03-23 16:07:24 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then (res => res.json())
+  .then (function(obj){
+    let body = document.querySelector("body")
+    body.append(obj.id)
+  })
+  .catch(function(error){
+    let body = document.querySelector("body")
+    body.append(error.message)
+  })
+}
